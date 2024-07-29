5926a0e2bacd0d6697cfc2e83fd288116955218c
TORY-SKY <ezevictor559@gmail.com>
2024-07-29 07:46:54 +0100

    installed some packages

--- src/common/firebase.ts
+++ src/common/firebase.ts
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,4 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider(); 
 const analytics = getAnalytics(app);
+
+export  {auth, provider}
