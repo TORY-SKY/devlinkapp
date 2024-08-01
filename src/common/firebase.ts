23b92c5c912e94965dad285c38137975bac68123
TORY-SKY <ezevictor559@gmail.com>
2024-08-01 15:13:34 +0100

    working on firebase

--- src/common/firebase.ts
+++ src/common/firebase.ts
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics"; 
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
+  apiKey: "import.meta.env.VITE_apiKey",
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
@@ -20,6 +20,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export { auth, provider };
+
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,3 @@
-
 import Navbar from "./Navbar";
 import Addlink from "./Addlink";
 
